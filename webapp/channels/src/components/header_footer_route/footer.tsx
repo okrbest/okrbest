2191387da1df98c76fed96e905b5e7c9e0f63262
sinyebin <dpqls0625@naver.com>
2025-09-01 23:18:48 +0900

    링크 okr.best로 변경

--- webapp/channels/src/components/header_footer_route/footer.tsx
+++ webapp/channels/src/components/header_footer_route/footer.tsx
@@ -26,7 +26,7 @@ const Footer = () => {
                 key='footer-copyright'
                 className='footer-copyright'
             >
-                {`© ${new Date().getFullYear()} Mattermost Inc.`}
+                {`© ${new Date().getFullYear()} OKR Best Inc.`}
             </span>
             {AboutLink && (
                 <ExternalLink
