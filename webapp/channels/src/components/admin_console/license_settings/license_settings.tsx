8958ab76f42a236476ecaf1b21e9e47b5f0c9274
sinyebin <dpqls0625@naver.com>
2025-09-27 00:59:12 +0900

    에러

--- webapp/channels/src/components/admin_console/license_settings/license_settings.tsx
+++ webapp/channels/src/components/admin_console/license_settings/license_settings.tsx
@@ -3,7 +3,7 @@
 
 import classNames from 'classnames';
 import React from 'react';
-import {FormattedMessage, defineMessages, useIntl} from 'react-intl';
+import {FormattedMessage, defineMessages} from 'react-intl';
 
 import type {StatusOK} from '@mattermost/types/client4';
 import type {ClientLicense, EnvironmentConfig} from '@mattermost/types/config';
@@ -272,28 +272,28 @@ export default class LicenseSettings extends React.PureComponent<Props, State> {
             {this.props.intl.formatMessage(
                 {id: 'admin.license.termsAndPolicy', defaultMessage: 'See also {termsLink} and {privacyLink}'},
                 {
-                    termsLink: this.createLink(AboutLinks.TERMS_OF_SERVICE, 
+                    termsLink: this.createLink(AboutLinks.TERMS_OF_SERVICE,
                         this.props.intl.formatMessage({id: 'admin.license.termsLink', defaultMessage: 'Enterprise Edition Terms of Use'})),
-                    privacyLink: this.createLink(AboutLinks.PRIVACY_POLICY, 
+                    privacyLink: this.createLink(AboutLinks.PRIVACY_POLICY,
                         this.props.intl.formatMessage({id: 'admin.license.privacyLink', defaultMessage: 'Privacy Policy'})),
-                }
+                },
             )}
         </div>
     );
 
     comparePlans = (this.props.license.SkuShortName === LicenseSkus.Entry ? null : (
         <div className='compare-plans-text'>
-        {this.props.intl.formatMessage(
-            {id: 'admin.license.comparePlans', defaultMessage: 'Curious about upgrading? {compareLink}'},
-            {
-                compareLink: this.createLink(
-                    CloudLinks.PRICING,
-                    this.props.intl.formatMessage({id: 'admin.license.compareLink', defaultMessage: 'Compare Plans'})
-                ),
-            }
-        )}
-    </div>
-    );
+            {this.props.intl.formatMessage(
+                {id: 'admin.license.comparePlans', defaultMessage: 'Curious about upgrading? {compareLink}'},
+                {
+                    compareLink: this.createLink(
+                        CloudLinks.PRICING,
+                        this.props.intl.formatMessage({id: 'admin.license.compareLink', defaultMessage: 'Compare Plans'}),
+                    ),
+                },
+            )}
+        </div>
+    ));
 
     render() {
         const {license, upgradedFromTE, isDisabled} = this.props;
