38688650ff3fc6bd07bb3f489f7a1fadc42e7c03
idevcus <idevcus@gmail.com>
2025-12-11 12:02:53 +0900

    pgvector 사용 준비

--- server/build/docker/postgres_node_database.sql
+++ server/build/docker/postgres_node_database.sql
@@ -1,2 +1,9 @@
 CREATE DATABASE mattermost_node_test;
 GRANT ALL PRIVILEGES ON DATABASE mattermost_node_test TO mmuser;
+
+-- Enable pgvector extension
+\c mattermost_test
+CREATE EXTENSION IF NOT EXISTS vector;
+
+\c mattermost_node_test
+CREATE EXTENSION IF NOT EXISTS vector;
