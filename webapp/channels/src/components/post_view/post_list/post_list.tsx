8ccc3c24bf9988ff03788513eff62167f3a7e41e
정건우(ignite) <doscmDev@gmail.com>
2026-02-03 09:11:12 +0900

    fix: 멤버 필터 변경 시 게시물 재로드 로직 추가 (#156)
    PostList 컴포넌트에서 filterUserIds 변경 감지하여 게시물 자동 재로드
    
    ## 수정 사항
    - PostList에 filterUserIds props 추가
    - componentDidUpdate에서 필터 변경 감지
    - 필터 변경 시 postsOnLoad 호출하여 게시물 재로드

--- webapp/channels/src/components/post_view/post_list/post_list.tsx
+++ webapp/channels/src/components/post_view/post_list/post_list.tsx
@@ -120,6 +120,11 @@ export interface Props {
     shouldStartFromBottomWhenUnread: boolean;
     hasInaccessiblePosts: boolean;
 
+    /*
+     * User IDs to filter posts by (member filtering)
+     */
+    filterUserIds: string[];
+
     dispatch: DispatchFunc;
 
     actions: {
@@ -211,6 +216,12 @@ export default class PostList extends React.PureComponent<Props, State> {
         if (this.props.channelId !== prevProps.channelId) {
             this.postsOnLoad(this.props.channelId);
         }
+
+        // 멤버 필터가 변경되었을 때 게시물 재로드
+        if (this.props.filterUserIds !== prevProps.filterUserIds) {
+            this.postsOnLoad(this.props.channelId);
+        }
+
         if (this.props.postListIds != null && prevProps.postListIds == null) {
             markAndMeasureChannelSwitchEnd(true);
         }
