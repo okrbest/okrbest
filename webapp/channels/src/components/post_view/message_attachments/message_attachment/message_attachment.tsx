54329a70c0eeacda12f6301a49e2a00132115318
Doug Lauder <wiggin77@warpmail.net>
2026-03-05 09:49:20 -0500

    MM-45293 Index all attachment content fields in Elasticsearch (#35454)
    * MM-45293 Index all Slack attachment content fields in Elasticsearch
    
      Previously only the "text" field from Slack attachments was indexed.
      This adds title, pretext, fallback, and field titles/values, making
      posts from integrations (JIRA, GitHub, CI bots) fully searchable.
    
    (cherry picked from commit 5d6e6733d594772e7cd289280462623b09acff47)
    
    Upstream: https://github.com/mattermost/mattermost/commit/5d6e6733d594772e7cd289280462623b09acff47

--- webapp/channels/src/components/post_view/message_attachments/message_attachment/message_attachment.tsx
+++ webapp/channels/src/components/post_view/message_attachments/message_attachment/message_attachment.tsx
@@ -272,7 +272,7 @@ export default class MessageAttachment extends React.PureComponent<Props, State>
         let rowPos = 0;
         let lastWasLong = false;
         let nrTables = 0;
-        const markdown = {markdown: false, mentionHighlight: false, atMentions: false};
+        const markdown = {...this.props.options, markdown: false, mentionHighlight: false, atMentions: false};
 
         fields.forEach((field, i) => {
             if (rowPos === 2 || !(field.short === true) || lastWasLong) {
@@ -319,6 +319,7 @@ export default class MessageAttachment extends React.PureComponent<Props, State>
                 >
                     <Markdown
                         message={String(field.value)}
+                        options={this.props.options}
                         postId={this.props.postId}
                     />
                 </td>,
@@ -407,6 +408,7 @@ export default class MessageAttachment extends React.PureComponent<Props, State>
                 <div className='attachment__thumb-pretext'>
                     <Markdown
                         message={attachment.pretext}
+                        options={options}
                         postId={this.props.postId}
                     />
                 </div>
@@ -477,6 +479,7 @@ export default class MessageAttachment extends React.PureComponent<Props, State>
                         <Markdown
                             message={attachment.title}
                             options={{
+                                ...options,
                                 atMentions: false,
                                 mentionHighlight: false,
                                 renderer: new LinkOnlyRenderer(),
