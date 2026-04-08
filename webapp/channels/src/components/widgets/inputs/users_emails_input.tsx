3ffe5e60168f50de67231bd400db19fccad9191c
Harrison Healey <harrisonmhealey@gmail.com>
2026-04-08 10:11:24 -0400

    MM-66887 Fix results in Invite to Team modal (#35936)
    * Fix a bug in decomposeKorean that caused it to only decompose the first character
    
    * MM-66887 Fix Invite To Team modal suggesting users one key press behind
    
    * Cherry-pick updates to ime.ts from another branch
    
    * And cherry-pick another bit
    
    * Remove broken and unnecessary onBlur method from UsersEmailsInput
    
    See https://github.com/mattermost/mattermost/pull/35936/changes#r3047500882 for more information
    
    (cherry picked from commit 220cd725ccee2b9a8022c2ae2620ca4db011d620)
    
    Upstream: https://github.com/mattermost/mattermost/commit/220cd725ccee2b9a8022c2ae2620ca4db011d620

--- webapp/channels/src/components/widgets/inputs/users_emails_input.tsx
+++ webapp/channels/src/components/widgets/inputs/users_emails_input.tsx
@@ -34,7 +34,6 @@ type Props = {
     ariaLabel: string;
     usersLoader: (search: string, callback: (users: UserProfile[]) => void) => Promise<UserProfile[]> | undefined;
     onUsersLoad?: (users: UserProfile[]) => void;
-    onBlur?: () => void;
     onChange: (change: Array<UserProfile | string>) => void;
     showError?: boolean;
     errorMessage?: MessageDescriptor;
@@ -367,7 +366,7 @@ export class UsersEmailsInput extends React.PureComponent<Props, State> {
         });
     };
 
-    optionsLoader = (_input: string, callback: (options: UserProfile[]) => void) => {
+    optionsLoader = (inputValue: string, callback: (options: UserProfile[]) => void) => {
         const customCallback = (options: UserProfile[]) => {
             this.setState({options});
             const accessibleProfiles = options.map((user: UserProfile) => ({...user, label: user.username}));
@@ -376,7 +375,7 @@ export class UsersEmailsInput extends React.PureComponent<Props, State> {
                 this.props.onUsersLoad(options);
             }
         };
-        const result = this.props.usersLoader(this.props.inputValue, customCallback);
+        const result = this.props.usersLoader(inputValue, customCallback);
         if (result && result.then) {
             result.then(customCallback);
         }
@@ -388,13 +387,6 @@ export class UsersEmailsInput extends React.PureComponent<Props, State> {
 
     onFocus = () => {
         this.selectRef.current?.onInputChange(this.props.inputValue, {action: 'set-value', prevInputValue: this.props.inputValue});
-    };
-
-    onBlur = () => {
-        this.selectRef.current?.onInputChange(this.props.inputValue, {action: 'input-blur', prevInputValue: this.state.prevValue});
-        if (this.props.onBlur) {
-            this.props.onBlur();
-        }
     };
 
     appendDelimitedValues = async (values: string, delimiter: RegExp = pasteDelimiter): Promise<number> => {
@@ -566,8 +558,7 @@ export class UsersEmailsInput extends React.PureComponent<Props, State> {
                     onInputChange={this.handleInputChange}
                     inputValue={this.props.inputValue}
                     openMenuOnFocus={true}
-                    onFocus={() => this.onFocus}
-                    onBlur={() => this.onBlur}
+                    onFocus={this.onFocus}
                     tabSelectsValue={true}
                     value={values}
                     aria-label={this.props.ariaLabel}
