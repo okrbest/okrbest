4a00d818bee4324830eb5ba42f1ea6d3b8fdd73c
Caleb Roseland <caleb@calebroseland.com>
2026-06-30 18:36:27 -0500

    MM-61199: Remove channelBookmarks feature flag (#37120)
    (cherry picked from commit cb136ac81e8576f398683d82d3406e9df71c0260)
    
    Upstream: https://github.com/mattermost/mattermost/commit/cb136ac81e8576f398683d82d3406e9df71c0260

--- e2e-tests/playwright/lib/src/server/default_config.ts
+++ e2e-tests/playwright/lib/src/server/default_config.ts
@@ -794,7 +794,6 @@ const defaultServerConfig: AdminConfig = {
         MoveThreadsEnabled: false,
         StreamlinedMarketplace: true,
         CloudDedicatedExportUI: false,
-        ChannelBookmarks: true,
         WebSocketEventScope: true,
         NotificationMonitoring: true,
         ExperimentalAuditSettingsSystemConsoleUI: true,
