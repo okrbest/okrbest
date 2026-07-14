bcff5533252387ee1eccfaa194028c98fb9a20c4
Jesse Hallam <jesse.hallam@gmail.com>
2026-07-14 12:38:53 -0300

    [MM-69592] Remove feature flag: OnboardingTourTips (#37475)
    Co-authored-by: Mattermost Build <build@mattermost.com>
    (cherry picked from commit a91ac6db9dfc0f50d7ab75ebb3506f698ab337a7)
    
    Upstream: https://github.com/mattermost/mattermost/commit/a91ac6db9dfc0f50d7ab75ebb3506f698ab337a7

--- e2e-tests/playwright/lib/src/server/default_config.ts
+++ e2e-tests/playwright/lib/src/server/default_config.ts
@@ -789,7 +789,6 @@ const defaultServerConfig: AdminConfig = {
         AppsEnabled: false,
         NormalizeLdapDNs: false,
         WysiwygEditor: false,
-        OnboardingTourTips: true,
         EnableExportDirectDownload: false,
         MoveThreadsEnabled: false,
         CloudDedicatedExportUI: false,
