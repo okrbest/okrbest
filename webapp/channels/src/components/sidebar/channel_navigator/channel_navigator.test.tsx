mattermost/mattermost

MM-67328 Bulk migrate Enzyme to RTL (M2 to M6) (#35068)
* migrate(enzyme): bulk migration to rtl

* updated per suggestion

---------

Co-authored-by: Mattermost Build <build@mattermost.com>

webapp/channels/src/components/sidebar/channel_navigator/channel_navigator.test.tsx
@@ -1,31 +1,33 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {shallow} from 'enzyme';
 import React from 'react';
 
-import BrowserOrAddChannelMenu from 'components/sidebar/sidebar_header/sidebar_browse_or_add_channel_menu';
+import {renderWithContext, screen} from 'tests/react_testing_utils';
 
 import ChannelNavigator from './channel_navigator';
-import type {Props} from './channel_navigator';
 
-let props: Props;
+// Mock child components
+jest.mock('../channel_filter', () => () => <div id='mock-channel-filter'/>);
 
 describe('Components/ChannelNavigator', () => {
-    beforeEach(() => {
-        props = {
-            showUnreadsCategory: true,
-            isQuickSwitcherOpen: false,
-            intl: {} as any,
-            actions: {
-                openModal: jest.fn(),
-                closeModal: jest.fn(),
-            },
-        };
-    });
+    const baseProps = {
+        showUnreadsCategory: true,
+        isQuickSwitcherOpen: false,
+        actions: {
+            openModal: jest.fn(),
+            closeModal: jest.fn(),
+        },
+    };
 
     it('should not show BrowserOrAddChannelMenu', () => {
-        const wrapper = shallow(<ChannelNavigator {...props}/>);
-        expect(wrapper.find(BrowserOrAddChannelMenu).length).toBe(0);
+        renderWithContext(<ChannelNavigator {...baseProps}/>);
+
+        // Component renders find channel button instead of BrowserOrAddChannelMenu
+        expect(screen.getByRole('button', {name: /find channels/i})).toBeInTheDocument();
+        expect(screen.getByText('Find channel')).toBeInTheDocument();
+
+        // Channel filter not shown when showUnreadsCategory is true
+        expect(document.querySelector('#mock-channel-filter')).not.toBeInTheDocument();
     });
 });

okrbest/okrbest

- select_team_item / sidebar_category 스냅샷 2건: Jest 30 포맷 차이가 아니라
  okrbest 자체 문구("Private team", "Create new direct message")를 반영하도록
  재생성.
- channel_navigator.test.tsx: 원 커밋의 접근성 이름 정규식(/find channels/i)이
  실제 aria-label("Channel Switcher")과 불일치하는 upstream 자체 결함(우리 fork
  고유 변경 아님) — 정규식을 /channel switcher/i로 수정.
(cherry picked from commit 51e64312750724bca2614a57a81dc167b8db1e16, adapted for okrbest)
Upstream: https://github.com/mattermost/mattermost/commit/51e64312750724bca2614a57a81dc167b8db1e16

webapp/channels/src/components/sidebar/channel_navigator/channel_navigator.test.tsx
@@ -1,31 +1,33 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {shallow} from 'enzyme';
 import React from 'react';
 
-import BrowserOrAddChannelMenu from 'components/sidebar/sidebar_header/sidebar_browse_or_add_channel_menu';
+import {renderWithContext, screen} from 'tests/react_testing_utils';
 
 import ChannelNavigator from './channel_navigator';
-import type {Props} from './channel_navigator';
 
-let props: Props;
+// Mock child components
+jest.mock('../channel_filter', () => () => <div id='mock-channel-filter'/>);
 
 describe('Components/ChannelNavigator', () => {
-    beforeEach(() => {
-        props = {
-            showUnreadsCategory: true,
-            isQuickSwitcherOpen: false,
-            intl: {} as any,
-            actions: {
-                openModal: jest.fn(),
-                closeModal: jest.fn(),
-            },
-        };
-    });
+    const baseProps = {
+        showUnreadsCategory: true,
+        isQuickSwitcherOpen: false,
+        actions: {
+            openModal: jest.fn(),
+            closeModal: jest.fn(),
+        },
+    };
 
     it('should not show BrowserOrAddChannelMenu', () => {
-        const wrapper = shallow(<ChannelNavigator {...props}/>);
-        expect(wrapper.find(BrowserOrAddChannelMenu).length).toBe(0);
+        renderWithContext(<ChannelNavigator {...baseProps}/>);
+
+        // Component renders find channel button instead of BrowserOrAddChannelMenu
+        expect(screen.getByRole('button', {name: /channel switcher/i})).toBeInTheDocument();
+        expect(screen.getByText('Find channel')).toBeInTheDocument();
+
+        // Channel filter not shown when showUnreadsCategory is true
+        expect(document.querySelector('#mock-channel-filter')).not.toBeInTheDocument();
     });
 });