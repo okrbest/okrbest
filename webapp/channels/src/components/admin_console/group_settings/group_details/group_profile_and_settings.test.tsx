hanzoai/team

MM-67957 H2-H9 Enzyme to RTL bulk migration (#35670)
* (test): h2-9 enzyme to rtl bulk migration

* update per comments and fix types

---------

Co-authored-by: Mattermost Build <build@mattermost.com>

webapp/channels/src/components/admin_console/group_settings/group_details/group_profile_and_settings.test.tsx
@@ -1,14 +1,15 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {shallow} from 'enzyme';
 import React from 'react';
+
+import {renderWithContext, screen} from 'tests/react_testing_utils';
 
 import {GroupProfileAndSettings} from './group_profile_and_settings';
 
 describe('components/admin_console/group_settings/group_details/GroupProfileAndSettings', () => {
     test('should match snapshot, with toggle off', () => {
-        const wrapper = shallow(
+        const {container} = renderWithContext(
             <GroupProfileAndSettings
                 displayname='GroupProfileAndSettings'
                 mentionname='GroupProfileAndSettings'
@@ -17,11 +18,16 @@ describe('components/admin_console/group_settings/group_details/GroupProfileAndS
                 onToggle={jest.fn()}
             />,
         );
-        expect(wrapper).toMatchSnapshot();
+
+        expect(container).toMatchSnapshot();
+        expect(screen.getByText('Enable Group Mentions')).toBeInTheDocument();
+
+        // Group Mention field should not be visible when toggle is off
+        expect(screen.queryByText('Group Mention:')).not.toBeInTheDocument();
     });
 
     test('should match snapshot, with toggle on', () => {
-        const wrapper = shallow(
+        const {container} = renderWithContext(
             <GroupProfileAndSettings
                 displayname='GroupProfileAndSettings'
                 mentionname='GroupProfileAndSettings'
@@ -30,6 +36,14 @@ describe('components/admin_console/group_settings/group_details/GroupProfileAndS
                 onToggle={jest.fn()}
             />,
         );
-        expect(wrapper).toMatchSnapshot();
+
+        expect(container).toMatchSnapshot();
+
+        // Group Mention field should be visible when toggle is on
+        expect(screen.getByText('Group Mention:')).toBeInTheDocument();
+
+        // Both inputs should be present (displayname and mentionname)
+        const inputs = screen.getAllByDisplayValue('GroupProfileAndSettings');
+        expect(inputs).toHaveLength(2);
     });
 });

okrbest/okrbest

RTL이 실제 DOM을 렌더하면서 드러난 okrbest 고유 문자열에 맞춰 스냅샷과
단언을 조정했다. 세 갈래다 — (1) 리브랜드 도메인·제품명(okr.best,
OKR.BEST), (2) db55f9fa43을 자체 spec으로 재구현하며 넘어오지 않은 영문
카피 차이("Enable Group Mention", teamOverrideSchemesBannerText 마침표
없음, "AD/LDAP Group sync"), (3) license_settings 테스트에 intl prop 주입.
(3)은 우리 55e167cad6이 LicenseSettings를 this.props.intl 기반으로 바꾸면서
테스트에 intl을 넘기지 않아 master에서도 이미 17건 실패하던 것을 함께
해소한 것이다. 프로덕션은 SchemaAdminSettings(injectIntl)가 {...this.props}로
intl을 내려주므로 런타임 영향은 없었다.
(cherry picked from commit 36bc59c1b768d8ed001b904ee341ffdb9f6f69f3, adapted for okrbest)
Upstream: https://github.com/mattermost/mattermost/commit/36bc59c1b768d8ed001b904ee341ffdb9f6f69f3

webapp/channels/src/components/admin_console/group_settings/group_details/group_profile_and_settings.test.tsx
@@ -1,14 +1,15 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {shallow} from 'enzyme';
 import React from 'react';
+
+import {renderWithContext, screen} from 'tests/react_testing_utils';
 
 import {GroupProfileAndSettings} from './group_profile_and_settings';
 
 describe('components/admin_console/group_settings/group_details/GroupProfileAndSettings', () => {
     test('should match snapshot, with toggle off', () => {
-        const wrapper = shallow(
+        const {container} = renderWithContext(
             <GroupProfileAndSettings
                 displayname='GroupProfileAndSettings'
                 mentionname='GroupProfileAndSettings'
@@ -17,11 +18,16 @@ describe('components/admin_console/group_settings/group_details/GroupProfileAndS
                 onToggle={jest.fn()}
             />,
         );
-        expect(wrapper).toMatchSnapshot();
+
+        expect(container).toMatchSnapshot();
+        expect(screen.getByText('Enable Group Mention')).toBeInTheDocument();
+
+        // Group Mention field should not be visible when toggle is off
+        expect(screen.queryByText('Group Mention:')).not.toBeInTheDocument();
     });
 
     test('should match snapshot, with toggle on', () => {
-        const wrapper = shallow(
+        const {container} = renderWithContext(
             <GroupProfileAndSettings
                 displayname='GroupProfileAndSettings'
                 mentionname='GroupProfileAndSettings'
@@ -30,6 +36,14 @@ describe('components/admin_console/group_settings/group_details/GroupProfileAndS
                 onToggle={jest.fn()}
             />,
         );
-        expect(wrapper).toMatchSnapshot();
+
+        expect(container).toMatchSnapshot();
+
+        // Group Mention field should be visible when toggle is on
+        expect(screen.getByText('Group Mention:')).toBeInTheDocument();
+
+        // Both inputs should be present (displayname and mentionname)
+        const inputs = screen.getAllByDisplayValue('GroupProfileAndSettings');
+        expect(inputs).toHaveLength(2);
     });
 });