1c29e9d7b702ef1737c62d2edec6d90051889229
sinyebin <dpqls0625@naver.com>
2026-08-18 14:18:54 +0900

    MM-38308 Remove remaining support for IE and pre-Chromium Edge (#36034)
    upstream은 webapp/channels/src/utils/user_agent.tsx를 삭제했으나, okrbest는
    Lexical 에디터의 use_orientation_handler가 이 파일의 isIosWeb()을 사용하므로
    (9fae005295) 파일을 남기고 isInternetExplorer·isEdge만 제거했다. 나머지 12개
    파일(IE/구형 Edge 분기, .browser--ie CSS, Edge 전용 알림 아이콘)은 upstream 그대로.
    
    (cherry picked from commit bf84301784777a6e08f9709ee882b0eac029437a, adapted for okrbest)
    Upstream: https://github.com/mattermost/mattermost/commit/bf84301784777a6e08f9709ee882b0eac029437a

--- webapp/channels/src/utils/user_agent.tsx
+++ webapp/channels/src/utils/user_agent.tsx
@@ -1,16 +1,10 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-export function isInternetExplorer(): boolean {
-    return window.navigator.userAgent.indexOf('Trident') !== -1;
-}
-
-export function isEdge(): boolean {
-    return window.navigator.userAgent.indexOf('Edge') !== -1;
-}
-
-// okrbest: upstream(f6341a17)은 use_orientation_handler와 함께 이 함수를 지웠으나,
+// okrbest: upstream(f6341a17)은 use_orientation_handler와 함께 isIosWeb을 지웠으나,
 // 우리는 그 훅을 Lexical 에디터용으로 유지하므로(9fae005295) 함수도 남긴다.
+// upstream(bf843017)이 IE·구형 Edge 정리로 이 파일 자체를 삭제했지만, 같은 이유로
+// 파일을 남기고 isInternetExplorer·isEdge만 제거한다.
 export function isIosWeb(): boolean {
     const userAgent = window.navigator.userAgent;
     const isIosSafari = (userAgent.indexOf('iPhone') !== -1 || userAgent.indexOf('iPad') !== -1) &&
--- webapp/channels/src/utils/utils.tsx
+++ webapp/channels/src/utils/utils.tsx
@@ -60,7 +60,6 @@ import type {A11yFocusEventDetail} from 'utils/constants';
 import DesktopApp from 'utils/desktop_api';
 import * as Keyboard from 'utils/keyboard';
 import {FOCUS_REPLY_POST, isPopoutWindow, sendToParent} from 'utils/popouts/popout_windows';
-import {isInternetExplorer, isEdge} from 'utils/user_agent';
 
 import {joinPrivateChannelPrompt} from './channel_utils';
 
@@ -1191,15 +1190,11 @@ export function fillRecord<T>(value: T, length: number): Record<number, T> {
 // Checks if a data transfer contains files not text, folders, etc..
 // Slightly modified from http://stackoverflow.com/questions/6848043/how-do-i-detect-a-file-is-being-dragged-rather-than-a-draggable-element-on-my-pa
 export function isFileTransfer(files: DataTransfer) {
-    if (isInternetExplorer() || isEdge()) {
-        return files.types != null && files.types.includes('Files');
-    }
-
     return files.types != null && (files.types.indexOf ? files.types.indexOf('Files') !== -1 : files.types.includes('application/x-moz-file'));
 }
 
 export function isUriDrop(dataTransfer: DataTransfer) {
-    if (isInternetExplorer() || isEdge() || isSafari()) {
+    if (isSafari()) {
         for (let i = 0; i < dataTransfer.items.length; i++) {
             if (dataTransfer.items[i].type === 'text/uri-list') {
                 return true;
