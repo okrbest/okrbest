hanzoai/team

MM-69172 Ensure note on post preview is always displayed for DM channels (#37232)
* MM-69172 Ensure note on post preview is always displayed for DM channels

* Replace snapshot tests for direct and group messages

* Fixed typos caused by last minute change

webapp/channels/src/components/post_view/post_message_preview/index.ts
@@ -8,12 +8,12 @@ import type {Dispatch} from 'redux';
 import type {PostPreviewMetadata} from '@mattermost/types/posts';
 
 import {General} from 'mattermost-redux/constants';
-import {isMyChannelAutotranslated, makeGetChannel} from 'mattermost-redux/selectors/entities/channels';
+import {getDirectTeammate, isMyChannelAutotranslated} from 'mattermost-redux/selectors/entities/channels';
 import {getConfig, isPermissionPoliciesEnabled} from 'mattermost-redux/selectors/entities/general';
 import {getPost, isPostPriorityEnabled} from 'mattermost-redux/selectors/entities/posts';
 import {get} from 'mattermost-redux/selectors/entities/preferences';
 import {getCurrentRelativeTeamUrl} from 'mattermost-redux/selectors/entities/teams';
-import {getUser} from 'mattermost-redux/selectors/entities/users';
+import {getUser, makeGetDisplayName} from 'mattermost-redux/selectors/entities/users';
 
 import {toggleEmbedVisibility} from 'actions/post_actions';
 import {isEmbedVisible} from 'selectors/posts';
@@ -32,7 +32,7 @@ export type OwnProps = {
 };
 
 function makeMapStateToProps() {
-    const getChannel = makeGetChannel();
+    const getDisplayName = makeGetDisplayName();
 
     return (state: GlobalState, ownProps: OwnProps) => {
         const config = getConfig(state);
@@ -50,7 +50,8 @@ function makeMapStateToProps() {
         }
 
         if (ownProps.metadata.channel_type === General.DM_CHANNEL) {
-            channelDisplayName = getChannel(state, ownProps.metadata.channel_id)?.display_name || '';
+            const teammate = getDirectTeammate(state, ownProps.metadata.channel_id);
+            channelDisplayName = getDisplayName(state, teammate?.id ?? '');
         }
 
         return {

okrbest/okrbest

index.ts 충돌은 import 한 줄이다. upstream은 isChannelAutotranslated를
isMyChannelAutotranslated로 개명했으나 그 커밋이 아직 미반영이라(우리는 자체
반영한 1e3530861b Autotranslation Frontend integration 기준의 이름을 쓴다)
우리 선택자 이름을 유지하고 upstream의 getDirectTeammate만 받았다.
(cherry picked from commit e0bfadc46f0a249ca9a571a9f3979fc2a48455f6, adapted for okrbest)
Upstream: https://github.com/mattermost/mattermost/commit/e0bfadc46f0a249ca9a571a9f3979fc2a48455f6
Co-Authored-By: Claude Opus 5 (1M context) <noreply@anthropic.com>

webapp/channels/src/components/post_view/post_message_preview/index.ts
@@ -8,12 +8,12 @@ import type {Dispatch} from 'redux';
 import type {PostPreviewMetadata} from '@mattermost/types/posts';
 
 import {General} from 'mattermost-redux/constants';
-import {isChannelAutotranslated, makeGetChannel} from 'mattermost-redux/selectors/entities/channels';
+import {getDirectTeammate, isChannelAutotranslated} from 'mattermost-redux/selectors/entities/channels';
 import {getConfig, isPermissionPoliciesEnabled} from 'mattermost-redux/selectors/entities/general';
 import {getPost, isPostPriorityEnabled} from 'mattermost-redux/selectors/entities/posts';
 import {get} from 'mattermost-redux/selectors/entities/preferences';
 import {getCurrentRelativeTeamUrl} from 'mattermost-redux/selectors/entities/teams';
-import {getUser} from 'mattermost-redux/selectors/entities/users';
+import {getUser, makeGetDisplayName} from 'mattermost-redux/selectors/entities/users';
 
 import {toggleEmbedVisibility} from 'actions/post_actions';
 import {isEmbedVisible} from 'selectors/posts';
@@ -32,7 +32,7 @@ export type OwnProps = {
 };
 
 function makeMapStateToProps() {
-    const getChannel = makeGetChannel();
+    const getDisplayName = makeGetDisplayName();
 
     return (state: GlobalState, ownProps: OwnProps) => {
         const config = getConfig(state);
@@ -50,7 +50,8 @@ function makeMapStateToProps() {
         }
 
         if (ownProps.metadata.channel_type === General.DM_CHANNEL) {
-            channelDisplayName = getChannel(state, ownProps.metadata.channel_id)?.display_name || '';
+            const teammate = getDirectTeammate(state, ownProps.metadata.channel_id);
+            channelDisplayName = getDisplayName(state, teammate?.id ?? '');
         }
 
         return {