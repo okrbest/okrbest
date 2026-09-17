hanzoai/team

chore: remove deprecated shared channels feature flags (#37154)
* chore: remove deprecated shared channels feature flags

* chore: remove sharedChannelsPluginsEnabled prop from channel header

* chore: remove enableSharedChannelsPlugins prop from file_search_results and post_message_view

* chore: drop hardcoded FeatureFlagEnableSharedChannelsPlugins from client config

* fix: remove unused channel prop from file_search_result_item

* fix: remove double blank line in file_search_result_item test

* remove redundant fragment wrapper in channel header

* Remove unused channelId prop from CodeBlock

webapp/channels/src/components/advanced_text_editor/advanced_text_editor.tsx
@@ -396,7 +396,7 @@ const AdvancedTextEditor = ({
         }]));
     }, [dispatch, currentUserId, getFormattingBarPreferenceName, isFormattingBarHidden]);
 
-    const pluginItems = usePluginItems(draft, textboxRef, handleDraftChange, channelId);
+    const pluginItems = usePluginItems(draft, textboxRef, handleDraftChange);
     const focusTextbox = useTextboxFocus(textboxRef, channelId, isRHS, canPost, wysiwygRef);
     const {
         rewriteMenuProps,

okrbest/okrbest

공유 채널 플래그 셋(EnableRemoteClusterService, EnableSharedChannelsPlugins,
EnableSharedChannelsMemberSync) 제거를 그대로 받았다. 충돌 4곳은 전부 우리
자체 변경 때문에 생긴 기계적 충돌이라 우리 형태를 유지한 채 upstream 훅만
적용했다.
충돌 해소 — 넷 다 우리 이름·프로퍼티를 지켰다
- advanced_text_editor.tsx: usePluginItems 호출에서 channelId 인자만 뺐다.
  우리는 textboxRef를 lexicalEditorRef로 개명한 상태라(Lexical 에디터 계보
  9fae0052) 줄 전체가 충돌했다. upstream 쪽에 있던 wysiwygRef는 받지 않는다 —
  제외한 TipTap 커밋(0fa2713b) 소산이다.
- use_plugin_items.tsx: usePluginVisibilityInSharedChannel 임포트를 지우고
  우리 LexicalTextEditorHandle 타입 임포트를 유지했다.
- channel_header/index.ts: sharedChannelsPluginsEnabled와 getFeatureFlagValue
  임포트를 지우고, 우리 showBotMessages 프로퍼티와 getMyChannelAutotranslation
  선택자 이름을 유지했다.
- channel_header_menu.tsx: pluginItemsVisible을 지우고 우리
  getChannelAutotranslation 선택자 이름을 유지했다.
autotranslation 선택자 개명(isChannelAutotranslated → isMyChannelAutotranslated)은
우리에게 미반영이라 이번에도 우리 이름을 지켰다 — docs/upstream-adapted-divergences.md의
Mattermost Blocks 항목 3번과 같은 줄이고 네 번째로 만난 자리다.
동작 영향 — 둘은 우리 배포에서 잠들어 있다
- EnableSharedChannelsPlugins(기본 true): 훅이 `!channel.shared || flag`라 이미
  항상 true였다. 변화 없음.
- EnableSharedChannelsMemberSync(기본 false): 멤버 동기화가 항상 꺼져 있었고,
  이제 원격 클러스터 서비스 유무로만 판정한다.
- EnableRemoteClusterService(기본 false): 관리자가 설정을 켜도 클라이언트에
  항상 "false"로 내려갔다. 이제 ConnectedWorkspacesSettings를 그대로 따른다.
뒤 둘은 ConnectedWorkspacesSettings.EnableSharedChannels와
EnableRemoteClusterService가 모두 기본 false라(config.go:1270·1274) 관리자가
커넥티드 워크스페이스를 켜지 않는 한 드러나지 않는다. 켠다면 설정이 실제로
작동하는 쪽이 옳다 — 그것이 이 커밋의 목적이다.
검증 (전부 master 기준선과 대조)
- go build ./... 종료코드 0
- go test ./platform/services/sharedchannel/ ./config/ → 둘 다 ok
- webapp tsc 29건 = 기준선 29건. 오류 파일 5개 중 이번 커밋이 건드리는 것은
  channel_header.test.tsx 하나뿐이고, 그 18건이 전부 showBotMessages 누락인데
  master에도 동일하다(우리 프로퍼티를 테스트에 반영 안 한 기존 부채).
  sharedChannel 관련 오류 0건.
- jest(code_block·file_search_results·profile_popover·post_message_view):
  10건 실패 4스냅샷 실패인데, master worktree에서 같은 스위트를 돌린 결과가
  10건 실패 4스냅샷 실패로 동일 — 기존 부채다.
(cherry picked from commit 9e3d8efc1a6

webapp/channels/src/components/advanced_text_editor/advanced_text_editor.tsx
@@ -287,7 +287,7 @@ const AdvancedTextEditor = ({
     }, [dispatch, currentUserId, getFormattingBarPreferenceName, isFormattingBarHidden]);
 
     useOrientationHandler(lexicalEditorRef, rootId);
-    const pluginItems = usePluginItems(draft, lexicalEditorRef, handleDraftChange, channelId);
+    const pluginItems = usePluginItems(draft, lexicalEditorRef, handleDraftChange);
     const focusTextbox = useTextboxFocus(lexicalEditorRef, channelId, isRHS, canPost);
     const {
         rewriteMenuProps,