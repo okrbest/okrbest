hanzoai/team

E2E/Playwright: Complete Rainforest browser test migration (batch 7, 121-142) (#37453)
* E2E/Playwright: Migrate RFQA browser tests (batch 4, 61-80)

Migrate the fourth batch of Rainforest QA browser tests into Playwright
(migration plan None-core #34-53, keys MM-T480..MM-T1257): 17 new/covered
specs across notifications, search, channel_settings, sidebar_left,
keyboard_shortcuts, and direct_messages_modal, plus 3 duplicate keys folded
into existing tests (MM-T480, MM-T1256, MM-T1257).

POM/infra: add a ChannelMenu page object for the channel header dropdown, an
EditChannelHeaderModal, a header field on the channel-settings info tab, a
channel-wide-mentions checkbox on NotificationsSettings, a getConfirmModal
helper, and a clickNotification browser-API mock helper. Give the channel
header menu and the confirm modal proper accessible names so they can be
targeted semantically. Notification specs run on all browsers now that the
Notification API stub works headless on Chromium and Firefox.

* E2E/Playwright: Migrate RFQA browser tests (batch 5, 81-100)

Migrate the fifth batch of Rainforest QA browser tests into Playwright across
keyboard_shortcuts, search, archived_channels, channel_settings,
plugin_marketplace, messaging, and emoji_picker (keys MM-T1247..MM-T2365),
folding duplicate coverage into existing keyboard-shortcut specs and
documenting skipped cases inline.

POM/infra: add AddPeopleToChannelModal and MarketplaceModal page objects,
manage/add member buttons on the channel members RHS, and an App Marketplace
entry on the global header; target the search "Jump" link through the existing
SearchResultsPanel POM.

Also fix a webapp bug where a post body disappeared when an inline edit was
cancelled mid-animation (found via the up-arrow edit shortcut spec).

* E2E/Playwright: Migrate RFQA browser tests (batch 6, 101-120)

Migrate the sixth batch of Rainforest QA browser tests into Playwright
(migration plan None-core #74-93, keys MM-T2547..MM-T5604) across multi_t

e2e-tests/playwright/specs/functional/system_console/user_attributes/user_attributes_management.spec.ts
@@ -1,0 +1,167 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import type {UserPropertyField} from '@mattermost/types/properties_user';
+
+import {expect, test} from '@mattermost/playwright-lib';
+
+import {
+    type CustomProfileAttribute,
+    deleteCustomProfileAttributes,
+    setupCustomProfileAttributeValuesForUser,
+} from '../../channels/custom_profile_attributes/helpers';
+
+function matchingOrder(actual: string[], expected: string[]) {
+    return actual.filter((value) => expected.includes(value));
+}
+
+/**
+ * @objective Verify navigating to the User Attributes page shows the empty state and disabled Save button.
+ */
+test('MM-T5745 navigates to user attributes page and displays empty state', {tag: '@user_attributes'}, async ({pw}) => {
+    await pw.ensureLicense();
+    await pw.skipIfNoLicense();
+    await pw.skipIfFeatureFlagNotSet('CustomProfileAttributes', true);
+    const {adminClient, adminUser} = await pw.initSetup();
+
+    // Custom profile attribute fields are global, so other specs may have left fields behind
+    // for reuse. Clear them to exercise the empty state, then restore them in `finally` so
+    // this test does not permanently wipe fields other specs depend on.
+    const existing = await adminClient.getCustomProfileAttributeFields();
+    try {
+        if (existing.length) {
+            const fields = Object.fromEntries(existing.map((field) => [field.id, field]));
+            await deleteCustomProfileAttributes(adminClient, fields);
+        }
+
+        const {systemConsolePage} = await pw.testBrowser.login(adminUser);
+        await systemConsolePage.goto();
+        await systemConsolePage.toBeVisible();
+
+        // # Navigate to User Attributes via the sidebar
+        await systemConsolePage.sidebar.systemAttributes.userAttributes.click();
+        const sp = systemConsolePage.systemProperties;
+
+        // * Verify the empty management page and disabled Save button
+        await sp.toBeVisible();
+        await expect(sp.addAttributeButton).toBeVisible();
+        await expect(sp.saveButton).toBeVisible();
+        await expect(sp.saveButton).toBeDisabled();
+    } finally {
+        for (const field of existing) {
+            try {
+                await adminClient.createCustomProfileAttributeField({
+                    name: field.name,
+                    type: field.type,
+                    attrs: field.attrs,
+                });
+            } catch (error) {
+                // eslint-disable-next-line no-console
+                console.log(`Failed to restore custom profile attribute field ${field.name}:`, error);
+            }
+        }
+    }
+});
+
+/**
+ * @objective Verify a system admin can add, edit, and delete a user attribute.
+ */
+test('MM-T5746 adds, edits, and deletes a user attribute', {tag: '@user_attributes'}, async ({pw}) => {
+    await pw.ensureLicense();
+    await pw.skipIfNoLicense();
+    await pw.skipIfFeatureFlagNotSet('CustomProfileAttributes', true);
+    const {adminClient, adminUser} = await pw.initSetup();
+    const {systemConsolePage} = await pw.testBrowser.login(adminUser);
+    const sp = systemConsolePage.systemProperties;
+    const originalName = `attr_${pw.random.id()}`.padEnd(40, '0').slice(0, 40);
+    const updatedName = `edited_${pw.random.id()}`;
+
+    // # Add and save a maximum-length user attribute
+    await sp.goto();
+    await sp.addAttribute();
+    await sp.lastNameInput().fill(originalName);
+    await sp.lastNameInput().press('Tab');
+    await sp.saveAndWaitForSettled();
+
+    // * Verify the new attribute persisted
+    let fields = await adminClient.getCustomProfileAttributeFields();
+    const created = fields.find((field) => field.name === originalName);
+    expect(created).toBeDefined();
+    await expect(sp.nameInputByValue(originalName)).toHaveValue(originalName);
+
+    // # Rename and save the attribute
+    await sp.nameInputByValue(originalName).fill(updatedName);
+    await systemConsolePage.page.keyboard.press('Tab');
+    await sp.saveAndWaitForSettled();
+    fields = await adminClient.getCustomProfileAttributeFields();
+    // * Verify the renamed attribute persisted
+    const updated = fields.find((field) => field.name === updatedName);
+    expect(updated).toBeDefined();
+
+    // # Delete and save the attribute
+    await sp.openDotMenu(updated!.id);
+    await sp.deleteAttribute();
+    await sp.confirmDeletion();
+    await sp.saveAndWaitForSettled();
+
+    // * Verify the attribute was deleted
+    fields = await adminClient.getCustomProfileAttributeFields();
+    expect(fields.find((field) => field.id === updated!.id)).toBeUndefined();
+});
+
+/**
+ * @objective Verify reordering attributes in the System Console updates profile settings and popover order.
+ */
+test(
+    'MM-T5766 propagates reordered user attributes to profile settings and popover',
+    {tag: '@user_attributes'},
+    async ({pw}) => {
+        await pw.ensureLicense();
+        await pw.skipIfNoLicense();
+        await pw.skipIfFeatureFlagNotSet('CustomProfileAttributes', true);
+        const {adminClient, adminUser, team, user} = await pw.initSetup();
+        const suffix = pw.random.id();
+        const labels = [`First ${suffix}`, `Second ${suffix}`, `Favorite Food ${suffix}`];
+        const fields: Record<string, UserPropertyField> = {};
+
+        for (const [index, label] of labels.entries()) {
+            const field = await adminClient.createCustomProfileAttributeField({
+                name: `reorder_${index}_${suffix}`,
+                type: 'text',
+                attrs: {display_name: label, sort_order: 1000 + index, visibility: 'always'},
+            } as any);
+            fields[field.id] = field;
+        }
+
+        const attributes: CustomProfileAttribute[] = labels.map((label, index) => ({
+            name: `reorder_${index}_${suffix}`,
+            type: 'text',
+            value: `Value ${index + 1}`,
+            attrs: {display_name: label, visibility: 'always'},
+        }));
+
+        await setupCustomProfileAttributeValuesForUser(adminClient, attributes, fields, user.id);
+        // # Move Favorite Food to the top in the System Console and save
+        const {systemConsolePage} = await pw.testBrowser.login(adminUser);
+        const sp = systemConsolePage.systemProperties;
+        await sp.goto();
+        await sp.reorderButtonByName(`reorder_2_${suffix}`).press('ArrowUp');
+        await sp.reorderButtonByName(`reorder_2_${suffix}`).press('ArrowUp');
+        await sp.saveAndWaitForSettled();
+
+        const expectedOrder = [labels[2], labels[0], labels[1]];
+        // * Verify the new order in profile settings
+        const {channelsPage} = await pw.testBrowser.login(user);
+        await channelsPage.goto(team.name, 'town-square');
+        const profileModal = await channelsPage.openProfileModal();
+        expect(matchingOrder(await profileModal.sectionHeadings.allTextContents(), labels)).toEqual(expectedOrder);
+        await profileModal.closeModal();
+
+        // # Open the user's profile popover
+        await channelsPage.postMessage(`Reordered attributes ${suffix}`);
+        const post = await channelsPage.getLastPost();
+        const popover = await channelsPage.openProfilePopover(post);
+        // * Verify the new order in the popover
+        expect(matchingOrder(await popover.attributeHeadings.allTextContents(), labels)).toEqual(expectedOrder);
+    },
+);

okrbest/okrbest

okrbest 맞춤: user_attributes_management.spec.ts의 UserPropertyField 임포트를
@mattermost/types/properties_user에서 properties로 되돌렸다. 타입을 분리한
property 시스템 v2 계보를 우리가 받지 않아 properties_user 모듈이 없고,
같은 타입을 쓰는 custom_profile_attributes/helpers.ts도 properties에서 가져온다.
(cherry picked from commit 51a2dbf1b93eb259a5b6631de8e80e917df999b4, adapted for okrbest)
Upstream: https://github.com/mattermost/mattermost/commit/51a2dbf1b93eb259a5b6631de8e80e917df999b4

e2e-tests/playwright/specs/functional/system_console/user_attributes/user_attributes_management.spec.ts
@@ -1,0 +1,170 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+// okrbest: upstream split these types into properties_user as part of the property
+// system v2 lineage we did not adopt; our tree still exports them from properties,
+// which is also what custom_profile_attributes/helpers.ts imports.
+import type {UserPropertyField} from '@mattermost/types/properties';
+
+import {expect, test} from '@mattermost/playwright-lib';
+
+import {
+    type CustomProfileAttribute,
+    deleteCustomProfileAttributes,
+    setupCustomProfileAttributeValuesForUser,
+} from '../../channels/custom_profile_attributes/helpers';
+
+function matchingOrder(actual: string[], expected: string[]) {
+    return actual.filter((value) => expected.includes(value));
+}
+
+/**
+ * @objective Verify navigating to the User Attributes page shows the empty state and disabled Save button.
+ */
+test('MM-T5745 navigates to user attributes page and displays empty state', {tag: '@user_attributes'}, async ({pw}) => {
+    await pw.ensureLicense();
+    await pw.skipIfNoLicense();
+    await pw.skipIfFeatureFlagNotSet('CustomProfileAttributes', true);
+    const {adminClient, adminUser} = await pw.initSetup();
+
+    // Custom profile attribute fields are global, so other specs may have left fields behind
+    // for reuse. Clear them to exercise the empty state, then restore them in `finally` so
+    // this test does not permanently wipe fields other specs depend on.
+    const existing = await adminClient.getCustomProfileAttributeFields();
+    try {
+        if (existing.length) {
+            const fields = Object.fromEntries(existing.map((field) => [field.id, field]));
+            await deleteCustomProfileAttributes(adminClient, fields);
+        }
+
+        const {systemConsolePage} = await pw.testBrowser.login(adminUser);
+        await systemConsolePage.goto();
+        await systemConsolePage.toBeVisible();
+
+        // # Navigate to User Attributes via the sidebar
+        await systemConsolePage.sidebar.systemAttributes.userAttributes.click();
+        const sp = systemConsolePage.systemProperties;
+
+        // * Verify the empty management page and disabled Save button
+        await sp.toBeVisible();
+        await expect(sp.addAttributeButton).toBeVisible();
+        await expect(sp.saveButton).toBeVisible();
+        await expect(sp.saveButton).toBeDisabled();
+    } finally {
+        for (const field of existing) {
+            try {
+                await adminClient.createCustomProfileAttributeField({
+                    name: field.name,
+                    type: field.type,
+                    attrs: field.attrs,
+                });
+            } catch (error) {
+                // eslint-disable-next-line no-console
+                console.log(`Failed to restore custom profile attribute field ${field.name}:`, error);
+            }
+        }
+    }
+});
+
+/**
+ * @objective Verify a system admin can add, edit, and delete a user attribute.
+ */
+test('MM-T5746 adds, edits, and deletes a user attribute', {tag: '@user_attributes'}, async ({pw}) => {
+    await pw.ensureLicense();
+    await pw.skipIfNoLicense();
+    await pw.skipIfFeatureFlagNotSet('CustomProfileAttributes', true);
+    const {adminClient, adminUser} = await pw.initSetup();
+    const {systemConsolePage} = await pw.testBrowser.login(adminUser);
+    const sp = systemConsolePage.systemProperties;
+    const originalName = `attr_${pw.random.id()}`.padEnd(40, '0').slice(0, 40);
+    const updatedName = `edited_${pw.random.id()}`;
+
+    // # Add and save a maximum-length user attribute
+    await sp.goto();
+    await sp.addAttribute();
+    await sp.lastNameInput().fill(originalName);
+    await sp.lastNameInput().press('Tab');
+    await sp.saveAndWaitForSettled();
+
+    // * Verify the new attribute persisted
+    let fields = await adminClient.getCustomProfileAttributeFields();
+    const created = fields.find((field) => field.name === originalName);
+    expect(created).toBeDefined();
+    await expect(sp.nameInputByValue(originalName)).toHaveValue(originalName);
+
+    // # Rename and save the attribute
+    await sp.nameInputByValue(originalName).fill(updatedName);
+    await systemConsolePage.page.keyboard.press('Tab');
+    await sp.saveAndWaitForSettled();
+    fields = await adminClient.getCustomProfileAttributeFields();
+    // * Verify the renamed attribute persisted
+    const updated = fields.find((field) => field.name === updatedName);
+    expect(updated).toBeDefined();
+
+    // # Delete and save the attribute
+    await sp.openDotMenu(updated!.id);
+    await sp.deleteAttribute();
+    await sp.confirmDeletion();
+    await sp.saveAndWaitForSettled();
+
+    // * Verify the attribute was deleted
+    fields = await adminClient.getCustomProfileAttributeFields();
+    expect(fields.find((field) => field.id === updated!.id)).toBeUndefined();
+});
+
+/**
+ * @objective Verify reordering attributes in the System Console updates profile settings and popover order.
+ */
+test(
+    'MM-T5766 propagates reordered user attributes to profile settings and popover',
+    {tag: '@user_attributes'},
+    async ({pw}) => {
+        await pw.ensureLicense();
+        await pw.skipIfNoLicense();
+        await pw.skipIfFeatureFlagNotSet('CustomProfileAttributes', true);
+        const {adminClient, adminUser, team, user} = await pw.initSetup();
+        const suffix = pw.random.id();
+        const labels = [`First ${suffix}`, `Second ${suffix}`, `Favorite Food ${suffix}`];
+        const fields: Record<string, UserPropertyField> = {};
+
+        for (const [index, label] of labels.entries()) {
+            const field = await adminClient.createCustomProfileAttributeField({
+                name: `reorder_${index}_${suffix}`,
+                type: 'text',
+                attrs: {display_name: label, sort_order: 1000 + index, visibility: 'always'},
+            } as any);
+            fields[field.id] = field;
+        }
+
+        const attributes: CustomProfileAttribute[] = labels.map((label, index) => ({
+            name: `reorder_${index}_${suffix}`,
+            type: 'text',
+            value: `Value ${index + 1}`,
+            attrs: {display_name: label, visibility: 'always'},
+        }));
+
+        await setupCustomProfileAttributeValuesForUser(adminClient, attributes, fields, user.id);
+        // # Move Favorite Food to the top in the System Console and save
+        const {systemConsolePage} = await pw.testBrowser.login(adminUser);
+        const sp = systemConsolePage.systemProperties;
+        await sp.goto();
+        await sp.reorderButtonByName(`reorder_2_${suffix}`).press('ArrowUp');
+        await sp.reorderButtonByName(`reorder_2_${suffix}`).press('ArrowUp');
+        await sp.saveAndWaitForSettled();
+
+        const expectedOrder = [labels[2], labels[0], labels[1]];
+        // * Verify the new order in profile settings
+        const {channelsPage} = await pw.testBrowser.login(user);
+        await channelsPage.goto(team.name, 'town-square');
+        const profileModal = await channelsPage.openProfileModal();
+        expect(matchingOrder(await profileModal.sectionHeadings.allTextContents(), labels)).toEqual(expectedOrder);
+        await profileModal.closeModal();
+
+        // # Open the user's profile popover
+        await channelsPage.postMessage(`Reordered attributes ${suffix}`);
+        const post = await channelsPage.getLastPost();
+        const popover = await channelsPage.openProfilePopover(post);
+        // * Verify the new order in the popover
+        expect(matchingOrder(await popover.attributeHeadings.allTextContents(), labels)).toEqual(expectedOrder);
+    },
+);